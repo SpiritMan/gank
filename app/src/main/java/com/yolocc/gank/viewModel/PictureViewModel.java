e3490050cff2a9ef6261ed9479be389a9065bd82
longle <longle@zhai.me>
2016-12-23 14:48:59 +0800

    添加下载图片功能

--- app/src/main/java/com/yolocc/gank/viewModel/PictureViewModel.java
+++ app/src/main/java/com/yolocc/gank/viewModel/PictureViewModel.java
@@ -1,24 +1,51 @@
 package com.yolocc.gank.viewModel;
 
+import android.content.Context;
+import android.content.Intent;
 import android.databinding.BindingAdapter;
 import android.databinding.ObservableInt;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.yolocc.gank.R;
+import com.yolocc.gank.model.GankApi;
+import com.yolocc.gank.model.GankService;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  */
 
-public class PictureViewModel {
+public class PictureViewModel implements ViewModel {
 
     public String mImageUrl;
 
+    public String desc;
+
     public ObservableInt mToolbarVisibility;
 
-    public PictureViewModel() {
+    private Subscription mSubscription;
+
+    private Context mContext;
+
+    public PictureViewModel(Context context) {
         mToolbarVisibility = new ObservableInt(View.VISIBLE);
+        this.mContext = context;
     }
 
     public void onImageClick(View view) {
@@ -29,11 +56,76 @@ public class PictureViewModel {
         }
     }
 
+    public void downloadImage() {
+        unSubscribe(mSubscription);
+        mSubscription = GankService.defaultInstance().create(GankApi.class)
+                .downloadPic(mImageUrl)
+                .subscribeOn(Schedulers.io())
+                .map(new Func1<ResponseBody, Uri>() {
+                    @Override
+                    public Uri call(ResponseBody responseBody) {
+                        final Bitmap bitmap =  BitmapFactory.decodeStream(responseBody.byteStream());
+                        File appDir = new File(Environment.getExternalStorageDirectory(),"GankImage");
+                        if(!appDir.exists()) {
+                            appDir.mkdir();
+                        }
+                        String imageName = desc + ".jpg";
+                        File file = new File(appDir,imageName);
+                        try {
+                            FileOutputStream outputStream = new FileOutputStream(file);
+                            assert bitmap != null;
+                            bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+                            outputStream.flush();
+                            outputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Uri uri = Uri.fromFile(file);
+                        return uri;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Uri>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(mContext, "无法下载图片", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNext(Uri uri) {
+                        // 通知图库更新
+                        Intent scannerIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
+                        mContext.sendBroadcast(scannerIntent);
+                        File appDir = new File(Environment.getExternalStorageDirectory(), "GankImage");
+                        String msg = "图片以保存到" + appDir.getAbsolutePath();
+                        Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
     @BindingAdapter({"pictureImageUrl"})
     public static void loadImage(ImageView view, String imageUrl) {
         Glide.with(view.getContext())
                 .load(imageUrl)
                 .error(R.mipmap.ic_launcher)
                 .into(view);
+    }
+
+    @Override
+    public void destroy() {
+        unSubscribe(mSubscription);
+        mSubscription = null;
+        mContext = null;
+    }
+
+    private void unSubscribe(Subscription subscription) {
+        if (subscription != null && !subscription.isUnsubscribed()) {
+            subscription.unsubscribe();
+        }
     }
 }
